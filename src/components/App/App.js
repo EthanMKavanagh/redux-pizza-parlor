9c26089a30f5339439eafcc511e06f34dc19486a
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-09-24 16:16:50 -0500

    small change

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import { connect } from "react-redux";
 import { Route, Link, HashRouter as Router } from "react-router-dom";
 import PizzaForm from "../PizzaForm/PizzaForm";
+import AddressForm from "../AddressForm/AddressForm";
+import Admin from "../Admin/Admin";
 
 class App extends Component {
 	state = {
@@ -36,26 +38,34 @@ class App extends Component {
 	};
 	render() {
 		return (
-			<div className='App'>
-				<header className='App-header'>
-					<h1 className='App-title'>Prime Pizza</h1>
-				</header>
-				<br />
-				<ul>
-					{this.state.pizzaList.map((pizza, index) => (
-						<div>
-							<li key={index}>
-								{pizza.name}
-								{pizza.description}
-								{pizza.price}
-								<button>Add Me</button>
-							</li>
-						</div>
-					))}
-				</ul>
-				<img src='images/pizza_photo.png' />
-				<p>Pizza is great.</p>
-			</div>
+			<Router>
+				<div className='App'>
+					<Route path='/admin'>
+						<Admin />
+					</Route>
+					<header className='App-header'>
+						<h1 className='App-title'>Prime Pizza</h1>
+					</header>
+					<br />
+					<ul>
+						{this.state.pizzaList.map((pizza, index) => (
+							<div>
+								<li key={index}>
+									{pizza.name}
+									{pizza.description}
+									{pizza.price}
+									<button>Add Me</button>
+								</li>
+								<Route path='/address-form' exact>
+									<AddressForm />
+								</Route>
+							</div>
+						))}
+					</ul>
+					<img src='images/pizza_photo.png' />
+					<p>Pizza is great.</p>
+				</div>
+			</Router>
 		);
 	}
 }
