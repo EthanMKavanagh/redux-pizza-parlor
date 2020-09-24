b990a72df5ca35c6c3dd06135e246f92662628f5
KStomberg <68118089+KStomberg@users.noreply.github.com>
2020-09-24 14:45:44 -0500

    GET req on admin side complete!

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Route, HashRouter as Router, Link} from 'react-router-dom';
 import './App.css';
+import Admin from '../Admin/Admin'
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
+        <Route path="/admin">
+          <Admin />
+        </Route>
         <header className="App-header">
           <h1 className="App-title">Prime Pizza</h1>
         </header>
@@ -13,6 +19,7 @@ class App extends Component {
         <img src="images/pizza_photo.png"/>
         <p>Pizza is great.</p>
       </div>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -5,17 +5,26 @@ import App from './components/App/App';
 import {createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
 
+// const adminReducer = (state = [], action) => {
+//     if (action.type === "GET_ORDERS") {
+//         let newOrdersList = action.payload;
+//         return newOrdersList;
+//     }
+//     return state
+// }
+
 const store = createStore(
     combineReducers({
-        homeScreen: homeScreenReducer,
-        addressForm: addressFormReducer,
-        checkout: checkoutReducer,
-        admin: adminReducer
+        // homeScreen: homeScreenReducer,
+        // addressForm: addressFormReducer,
+        // checkout: checkoutReducer,
+        // admin: adminReducer
     })
 );
 
+
 ReactDOM.render(
-    <Provider>
+    <Provider store={store}>
         <App />
     </Provider>,
      document.getElementById('root')
